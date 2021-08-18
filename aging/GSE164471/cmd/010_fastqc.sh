f73cdba41784aceddccf21d9e8f6f46d3e89969b
emmamk <michitsuruta7@gmail.com>
2021-08-18 14:52:46 -0700

    added FastQC lines on fastp files

--- aging/GSE164471/cmd/010_fastqc.sh
+++ aging/GSE164471/cmd/010_fastqc.sh
@@ -7,9 +7,9 @@ TARGET="GSE164471"  # check
 PROJECTDIR=/Volumes/HDD24TB/MetaAnalysisProject_Apr2021
 TARGETDIR=${PROJECTDIR}/${TYPE}/${TARGET}
 
-# fastq directory to be FastQC  ## check
-FILEDIR=${TARGETDIR}/fastq
-# FILEDIR=${TARGETDIR}/fastp
+# fastq directory to be FastQC  ## check which files to be on FastQC
+# FILEDIR=${TARGETDIR}/fastq
+FILEDIR=${TARGETDIR}/fastp
 # FILEDIR=${TARGETDIR}/sickle
 
 # fastqc directory
@@ -23,8 +23,14 @@ FILEIDS=${PROJECTDIR}/${TYPE}/${TARGET}/qclist.txt  # SRR_Acc_List.txt
 cd ${FILEDIR}
 cat $FILEIDS | while read line; do
     echo FastQC: ${line} "(${TARGET})"
+    # raw data
+    # fastqc -t 8 --nogroup -o ${OUTDIR} ${line}*.fastq.gz
+    # echo number of FastQC files: `ls ${OUTDIR}/*fastqc.zip | wc -l`
+    
+    # after fastp
     fastqc -t 8 --nogroup -o ${OUTDIR} ${line}*.fastq.gz
-    echo number of FastQC files: `ls ${OUTDIR}/*fastqc.zip | wc -l`
+    echo number of FastQC files: `ls ${OUTDIR}/*.fastp.fastqc.zip | wc -l`
+    
     echo "\n" 
 done
 
