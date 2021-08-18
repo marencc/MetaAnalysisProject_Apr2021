8ea94b81ecc3c4e2760f5134804b1c9a771696b8
emmamk <michitsuruta7@gmail.com>
2021-08-18 11:49:20 -0700

    added timestamps and removed  from

--- aging/GSE129643/cmd/020_fastp.sh
+++ aging/GSE129643/cmd/020_fastp.sh
@@ -3,30 +3,37 @@
 # adapter trimming is enabled by default
 # detecting adapter is TRUE for single end by default
 
+# time
+script_started=`date +%s`
+
 # targets
-TYPE="inactive"  # check
-TARGET="GSE113165"  # check
+TYPE="aging"  # check
+TARGET="GSE129643"  # check
+PROJECTDIR=/Volumes/HDD24TB/MetaAnalysisProject_Apr2021
+TARGETDIR=${PROJECTDIR}/${TYPE}/${TARGET}
 
 # fastq directory
-FASTQDIR="/Volumes/HDD24TB/${TARGET}"
+FASTQDIR=${TARGETDIR}/fastq
 
 # output directory
-mkdir -p ${FASTQDIR}/fastp
-FASTPDIR="${FASTQDIR}/fastp"
+mkdir -p ${TARGETDIR}/fastp
+FASTPDIR=${TARGETDIR}/fastp
 
 # file ids
-PROJECTDIR="/Users/Emma/Documents/Bioinformatics/DEG/MetaAnalysisProject_Apr2021"
-FILEIDS="${PROJECTDIR}/${TYPE}/${TARGET}/SRR_Acc_List.txt"
+FILEIDS=${TARGETDIR}/SRR_Acc_List.txt
+FILES=`cat ${FILEIDS} | wc -l`
 
 cd ${FASTPDIR}
 mkdir -p html json
+count=1
 cat $FILEIDS | while read line; do
-    echo fastp: ${line}
+    SECONDS=0
+    echo `date "+%m/%d/%Y %H:%M:%S"` fastp ${count} /${FILES}: ${line} "(${TARGET})"
     # single end
     # -3: enable per read cutting by quality in tail (3'), default is disabled
     # (WARNING: this will interfere deduplication for SE data)
     fastp -i ${FASTQDIR}/${line}.fastq.gz -o ${line}.fastp.fastq.gz \
-    -h html/${line}.report.html -j json/${line}.report.json --thread 4 -q 20 
+    -h html/${line}.report.html -j json/${line}.report.json --thread 8 -q 20 
     
     # pair end
     # fastp -i ${FASTQDIR}/${line}_1.fastq.gz -I ${FASTQDIR}/${line}_2.fastq.gz -3 \
@@ -34,9 +41,27 @@ cat $FILEIDS | while read line; do
     # --detect_adapter_for_pe \
     # -h html/${line}.report.html -j json/${line}.report.json \
     # -q 20 --trim_tail1 1 --trim_tail2 1 -l 20 --thread 4
-    echo number of fastp files: `ls /*.fastp.fastq.gz | wc -l`
+    echo `date "+%m/%d/%Y %H:%M:%S"` ${line} finished
+    
+    # processed time for one file
+    echo `date "+%m/%d/%Y %H:%M:%S"` finished ${line}
+    h=$(($SECONDS/3600))
+    m=$((($SECONDS/60)%60))
+    s=$(($SECONDS%60))
+    echo processed time: ${h}:${m}:${s}
+    
+    # elapsed time so far
+    elapsed_time=`date +%s`
+    elapsed=$((elapsed_time - script_started))
+    eh=$(($elapsed/3600))
+    em=$((($elapsed/60)%60))
+    es=$(($elapsed%60))
+    echo elapsed: ${eh}:${em}:${es}
+    
+    echo number of fastp files: `ls *.fastp.fastq.gz | wc -l` /${FILES}
+    count=$((count+1))
     echo "\n"
 done
 
-echo finished
+echo `date "+%m/%d/%Y %H:%M:%S"` fastp finished "\n"
 
